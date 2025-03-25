14881fb05bb52a38ba1be86d1a1b87d5a1c41d20
A <112488521+sowinskv@users.noreply.github.com>
2025-03-25 11:50:48 +0100

    back to the best pred

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,19 +1,19 @@
-from __future__ import annotations
-
 import os
+
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
-from sklearn.model_selection import cross_val_score, train_test_split
 from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import mean_squared_error
 from datetime import datetime
 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.impute import SimpleImputer
 import pickle
 import optuna
+import optuna.integration
 import re
-from sklearn.ensemble import IsolationForest
-import category_encoders as ce
 
 
 class ModelTrainer:
@@ -21,55 +21,42 @@ class ModelTrainer:
         self.results_path = results_path or "data\\results"
         self.data_file_paths = data_file_paths or ["data\\raw\\sales_ads_train.csv"]
         self.target_variable = 'Cena'
-        self.encoder = None
+        self.ohe = None # 1 one hot encoder for all data
 
     def train(self):
         data = pd.concat([pd.read_csv(file) for file in self.data_file_paths], ignore_index=True)
-        X, y = self.prepare_data(data, drop_rows=False, fit_encoder=True)
+        X, y = self.prepare_data(data, False, True) # todo change to False when making final predictions
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-        X_train, y_train = self.remove_outliers_isolation_forest(X_train, y_train, contamination=0.05)
 
         self.train_lightgbm_regressor(X_train, y_train, X_test, y_test)
 
-    def remove_outliers_isolation_forest(self, X: pd.DataFrame, y: pd.Series, contamination: float = 0.01) -> tuple[pd.DataFrame, pd.Series]:
-        iso = IsolationForest(contamination=contamination, random_state=42)
-        iso.fit(X)
-        preds = iso.predict(X)  # 1 for inliners, -1 for outliers
-        mask = preds != -1
-        X_clean = X[mask]
-        y_clean = y[mask]
-        return X_clean, y_clean
 
-    def prepare_data(
-        self,
-        data: pd.DataFrame,
-        drop_rows: bool = True,
-        fit_encoder: bool = False
-    ) -> tuple[pd.DataFrame, pd.Series | None]:
-        # cleaning, imputation:
+    def prepare_data(self, data: pd.DataFrame, drop_rows: bool = True, fit_encoder: bool = False) -> tuple[pd.DataFrame, pd.Series | None]:
         if drop_rows:
             data = data.dropna()
         else:
-            missing_threshold = 0.05  # imputing only columns with more than 5% missing values
+            # impute missing values
+            missing_threshold = 0.05  # only impute columns with more than 5% missing values
             numeric_cols = data.select_dtypes(include=['float64', 'int64']).columns
             numeric_to_impute = [col for col in numeric_cols if data[col].isnull().mean() > missing_threshold]
+
             categorical_cols = data.select_dtypes(include=['object', 'category']).columns
             categorical_to_impute = [col for col in categorical_cols if data[col].isnull().mean() > missing_threshold]
 
             if numeric_to_impute:
                 num_imputer = SimpleImputer(strategy='mean')
                 data[numeric_to_impute] = num_imputer.fit_transform(data[numeric_to_impute])
+
             if categorical_to_impute:
                 cat_imputer = SimpleImputer(strategy='most_frequent')
                 data[categorical_to_impute] = cat_imputer.fit_transform(data[categorical_to_impute])
 
-        # date conversion
+
         for col in ["Data_pierwszej_rejestracji", "Data_publikacji_oferty"]:
             data[col] = pd.to_datetime(data[col], errors='coerce', format='%d/%m/%Y')
+
         data["Wiek_samochodu_lata"] = (data["Data_publikacji_oferty"] - data["Data_pierwszej_rejestracji"]).dt.days // 365
 
-        # price conversion
         conversion_rates = {'EUR': 4.18, 'PLN': 1.0}
         if 'Cena' in data.columns:
             data['Cena'] = data.apply(
@@ -77,14 +64,15 @@ class ModelTrainer:
                 axis=1
             )
 
-        # category encoding
+        # data['Marka_pojazdu_freq'] = data['Marka_pojazdu'].map(data['Marka_pojazdu'].value_counts()/len(data))
+        # data['Model_pojazdu_freq'] = data['Model_pojazdu'].map(data['Model_pojazdu'].value_counts()/len(data))
+
         categorical_columns = [
             "Waluta", "Stan", "Marka_pojazdu", "Model_pojazdu", "Wersja_pojazdu",
             "Generacja_pojazdu", "Rodzaj_paliwa", "Naped", "Skrzynia_biegow",
             "Typ_nadwozia", "Kolor", "Kraj_pochodzenia", "Lokalizacja_oferty"
         ]
 
-        # optional: feature extraction from 'Wyposazenie'
         if 'Wyposazenie' in data.columns:
             import ast
             from sklearn.preprocessing import MultiLabelBinarizer
@@ -93,43 +81,65 @@ class ModelTrainer:
                     return ast.literal_eval(x)
                 except Exception:
                     return []
+
             data['wyposazenie_list'] = data['Wyposazenie'].apply(parse_equipment)
             mlb = MultiLabelBinarizer(sparse_output=True)
             equipment_matrix = mlb.fit_transform(data['wyposazenie_list'])
             equipment_df = pd.DataFrame.sparse.from_spmatrix(equipment_matrix, columns=mlb.classes_)
-            common_features = equipment_df.columns[equipment_df.sum() > 50]  # opcjonalnie
+            common_features = equipment_df.columns[equipment_df.sum() > 50] # optional
             equipment_df = equipment_df[common_features]
             data = pd.concat([data.drop(columns=["Wyposazenie", "wyposazenie_list"]), equipment_df], axis=1)
 
-        # mapping binary features
+        def make_unique(columns):
+            seen = {}
+            new_cols = []
+            for col in columns:
+                if col in seen:
+                    seen[col] += 1
+                    new_col = f"{col}_{seen[col]}"
+                else:
+                    seen[col] = 0
+                    new_col = col
+                new_cols.append(new_col)
+            return new_cols
+
+
+        # encode
+        if fit_encoder or self.ohe is None:
+            self.ohe = OneHotEncoder(handle_unknown='ignore', sparse_output=True)
+            ohe_array = self.ohe.fit_transform(data[categorical_columns])
+        else:
+            ohe_array = self.ohe.transform(data[categorical_columns])
+        ohe_df = pd.DataFrame.sparse.from_spmatrix(
+            ohe_array,
+            columns=self.ohe.get_feature_names_out(categorical_columns)
+        )
+        ohe_df.columns = [re.sub(r'\W+', '_', col).strip('_') for col in ohe_df.columns]
+        ohe_df.columns = make_unique(ohe_df.columns)
+
+        data = pd.concat([data.drop(columns=categorical_columns), ohe_df], axis=1)
+        numeric_cols = data.select_dtypes(include=['float64', 'int64']).columns
+        data[numeric_cols] = data[numeric_cols].fillna(0)
+
         data["Pierwszy_wlasciciel"] = data["Pierwszy_wlasciciel"].replace({"Yes": 1, "No": 0})
 
-        if self.target_variable in data.columns:
-            y = np.log1p(data[self.target_variable])  # log transform
-        else:
-            y = None
-
-        # target encoding
-        if fit_encoder:
-            self.encoder = ce.TargetEncoder(cols=categorical_columns)
-            data[categorical_columns] = self.encoder.fit_transform(data[categorical_columns], y)
-        else:
-            data[categorical_columns] = self.encoder.transform(data[categorical_columns])
-
-        # delete columns
         data = data.drop(columns=["Data_pierwszej_rejestracji", "Data_publikacji_oferty"])
+
         if self.target_variable in data.columns:
             X = data.drop(columns=[self.target_variable, "ID"])
+            y = np.log1p(data[self.target_variable]) # log scaling
+
         else:
+            # when we prepare test data there is no target variable
             X = data
+            y = None
 
         return X, y
 
+
     def train_lightgbm_regressor(self, X_train, y_train, X_test, y_test):
-        # splitting 90/10
-        X_tr, X_val, y_tr, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=42)
-        train_data = lgb.Dataset(X_tr, label=y_tr)
-        valid_data = lgb.Dataset(X_val, label=y_val, reference=train_data)
+
+        train_data = lgb.Dataset(X_train, label=y_train)
 
         def objective(trial):
             params = {
@@ -150,8 +160,9 @@ class ModelTrainer:
                 nfold=5,
                 num_boost_round=1000,
                 seed=42,
-                stratified=False
+                stratified = False # basic kfold for continous (log) target
             )
+
             return cv_results['valid rmse-mean'][-1]
 
         # optimize hyperparameters
@@ -168,54 +179,56 @@ class ModelTrainer:
             'seed': 42
         })
 
-        # training the final model (callbacks for early stopping)
-        final_model = lgb.train(
+        cv_results = lgb.cv(
             best_params,
             train_data,
+            nfold=5,
             num_boost_round=1000,
-            valid_sets=[valid_data],
-            callbacks=[lgb.early_stopping(stopping_rounds=50)]
+            seed=42,
+            stratified=False
         )
-        print("Best iteration:", final_model.best_iteration)
+        best_iteration = len(cv_results['valid rmse-mean'])
 
-        # cross validation
-        lgbm_model = lgb.LGBMRegressor()
-        lgbm_model._Booster = final_model
+        final_model = lgb.train(best_params, train_data, num_boost_round=best_iteration)
 
-        test_cv_scores = cross_val_score(lgbm_model, X_test, y_test, cv=5, scoring="neg_root_mean_squared_error")
-        train_cv_scores = cross_val_score(lgbm_model, X_train, y_train, cv=5, scoring="neg_root_mean_squared_error")
-        print("%0.2f accuracy with a standard deviation of %0.2f test" % (test_cv_scores.mean(), test_cv_scores.std()))
-        print("%0.2f accuracy with a standard deviation of %0.2f train" % (train_cv_scores.mean(), train_cv_scores.std()))
+        # eval on the held-out test set
+        y_pred = final_model.predict(X_test)
+        rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+        print(f'Final RMSE on test set: {rmse:.4f}')
+
 
         output_folder_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
         os.mkdir(f"{self.results_path}\\{output_folder_name}")
-        with open(f"{self.results_path}\\{output_folder_name}\\scores.txt", "a") as f:
-            f.write("%0.2f accuracy with a standard deviation of %0.2f test\n" % (test_cv_scores.mean(), test_cv_scores.std()))
-            f.write("%0.2f accuracy with a standard deviation of %0.2f train" % (train_cv_scores.mean(), train_cv_scores.std()))
+
         study.trials_dataframe().to_csv(f"{self.results_path}\\{output_folder_name}\\optuna_trials.csv", index=False)
 
-        # model saving
+        # save model
         with open(f'{self.results_path}\\{output_folder_name}\\lgbm_model_{output_folder_name}.pkl', 'wb') as f:
             pickle.dump(final_model, f)
 
-        # feature importance
-        lgb.plot_importance(final_model, importance_type='gain', max_num_features=30, figsize=(10, 10))
+        # save feature importance plot
+        lgb.plot_importance(final_model, importance_type='gain', figsize=(10, 10))
         plt.savefig(f"{self.results_path}\\{output_folder_name}\\feature_importance_{output_folder_name}.png")
 
-        # prediction on test data
-        X_test_raw = pd.read_csv("../data/raw/sales_ads_test.csv")
-        currencies = X_test_raw["Waluta"].copy()
-        X_test_prepared, _ = self.prepare_data(X_test_raw, drop_rows=False, fit_encoder=False)
-        test_pred = final_model.predict(X_test_prepared.drop(columns=["ID"]))
-        test_pred = np.expm1(test_pred)  # reverse log transform
+        #todo add eval on our test set
+
+        X_test = pd.read_csv("../data/raw/sales_ads_test.csv")
+        currencies = X_test["Waluta"].copy()
+        X_test, _ = self.prepare_data(X_test, drop_rows=False)
+        test_pred = final_model.predict(X_test.drop(columns=["ID"]))
+        test_pred = np.expm1(test_pred)  # reverse log scaling
+
         mask_eur = currencies == 'EUR'
         test_pred[mask_eur] = test_pred[mask_eur] / 4.18
 
-        test_prediction_df = pd.DataFrame({"ID": X_test_prepared.index + 1, "Cena": test_pred})
+        # save csv with columns ID and Cena - final data for upload
+        test_prediction_df = pd.DataFrame({"ID": X_test.index + 1, "Cena": test_pred})  # start ID from 1
         print(test_prediction_df.head())
         test_prediction_df.to_csv(f"{self.results_path}\\{output_folder_name}\\kaggle_upload_prediction.csv", index=False)
 
 
-if __name__ == "__main__":
-    model_trainer = ModelTrainer('../data/results/', ["../data/raw/sales_ads_train.csv"])
-    model_trainer.train()
+
+
+model_trainer = ModelTrainer('../data/results/', ["../data/raw/sales_ads_train.csv"])
+model_trainer.train()
